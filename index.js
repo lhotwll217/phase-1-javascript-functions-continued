cf6860182db73d4fc45cd19e55f347e52b750346
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-30 17:20:37 -0400

    commit tree 63c353ab720884376cbca3fe3b283e07e249764f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(action = "go to the office") {
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function (compliment = "special") {
+        return `You are ${flair}${compliment}${flair}!`
+    }
+}
